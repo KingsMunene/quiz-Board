9a56bf9606aca5c81ac666847c65ae0d55b15536
Kings Munene <gmaxkings@gmail.com>
2019-02-25 14:36:04 +0300

    add scripts.js layout

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-  $("button#btn").click(function results(event){
-    var marks = 0;
-    $("input:radio[name=ans2]:checked,input:radio[name=ans12]:checked,input:radio[name=ans23]:checked,input:radio[name=ans34]:checked,input:radio[name=ans15]:checked").each(function(){
-      count=marks += parseInt($(this).val());
-      percentage = (count/50)*100;
+  $(".btn").submit(function(event){
+    event.preventDefault();
+    var answer1 = parseInt($("input:radio[name = input1]:checked").val());
+    var answer2 = parseInt($("input:radio[name = input2]:checked").val());
+    var answer3 = parseInt($("input:radio[name = input3]:checked").val());
+    var answer4 = parseInt($("input:radio[name = input4]:checked").val());
+    var answer5 = parseInt($("input:radio[name = input5]:checked").val());
+
+    var total = answer1 + answer2 +answer3 +answer4 + answer5;
+    document.getElementById('result').innerHTML ="Your score is" + total;
     });
   });
